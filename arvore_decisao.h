de1e626bd093166a1b18e18c3659e0739b0471ff
Thiago Fernandes Dias <thiago.fernandesdias235@gmail.com>
2025-11-21 09:57:57 -0300

    Formatação.

--- arvore_decisao.h
+++ arvore_decisao.h
@@ -5,18 +5,18 @@
 
 typedef enum
 {
-	TIPO_CONSUMO_MES_ANTERIOR,
-	TIPO_VISITAS_MES_ANTERIOR,
-	TIPO_CATEGORIA // Folha
+        TIPO_CONSUMO_MES_ANTERIOR,
+        TIPO_VISITAS_MES_ANTERIOR,
+        TIPO_CATEGORIA // Folha
 } TipoNo;
 
 typedef struct NoDecisao
 {
-	TipoNo tipo;
-	float valor_corte;			// Usado para CONSUMO e FREQUENCIA
-	char categoria[50];			// Usado apenas para TIPO_CATEGORIA
-	struct NoDecisao *esquerda; // Caminho se (atributo < valor_corte) for VERDADEIRO
-    struct NoDecisao *direita;  // Caminho se (atributo < valor_corte) for FALSO
+        TipoNo tipo;
+        float valor_corte;          // Usado para CONSUMO e FREQUENCIA
+        char categoria[50];         // Usado apenas para TIPO_CATEGORIA
+        struct NoDecisao *esquerda; // Caminho se (atributo < valor_corte) for VERDADEIRO
+        struct NoDecisao *direita;  // Caminho se (atributo < valor_corte) for FALSO
 } NoDecisao;
 
 NoDecisao *criar_no_decisao(TipoNo tipo, float valor, NoDecisao *esq, NoDecisao *dir);
